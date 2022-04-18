fb26f13dd0d77f9e1138c226d274ac9049ba8aa2
Jay <jayhomzhou08@gmail.com>
2022-04-18 14:59:10 +0800

    jay
    some changes for J projects.

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/leif-ibsen/ASN1", from: "1.2.1"),
-        .package(url: "https://github.com/leif-ibsen/BigInt", from: "1.2.5"),
+        .package(url: "https://github.com/jayhomzhou/JASN1", from: "1.0.0"),
+        .package(url: "https://github.com/jayhomzhou/JBigInt", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftECC",
-            dependencies: ["ASN1", "BigInt"]),
+            dependencies: ["ASN1", "JBigInt"]),
         .testTarget(
             name: "SwiftECCTests",
             dependencies: ["SwiftECC"]),
